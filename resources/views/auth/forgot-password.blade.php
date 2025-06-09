3eb488706ddd3e3903615a2563ed47a6a6abb5dc
Ayelén Escalante <daianaayelenescalante@gmail.com>
2025-06-09 01:39:35 -0300

    modificaciones en register

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,6 +1,6 @@
 <x-guest-layout>
     <div class="mb-4 text-sm text-gray-600">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+        {{ __('¿Olvidaste tu contraseña? No te preocupes. Ingresá tu correo electrónico y te mandamos un enlace para que puedas crear una nueva.') }}
     </div>
 
     <!-- Session Status -->
@@ -18,7 +18,7 @@
 
         <div class="flex items-center justify-end mt-4">
             <x-primary-button>
-                {{ __('Email Password Reset Link') }}
+                {{ __('Recibir enlace por email') }}
             </x-primary-button>
         </div>
     </form>
